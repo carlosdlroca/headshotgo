5e2c93e098bad0df5faafd793d7ab5683f5d93a7
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-08-20 18:52:46 -0700

    Edit Links

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -3,7 +3,9 @@ import { FooterContainer, Copyright } from "./Styles"
 
 import TwitterSvg from "./twitter.svg"
 import InstagramSvg from "./instagram.svg"
-import FacebookSvg from "./facebook.svg"
+import LinkedInSvg from "./linkedin.svg"
+import YoutubeSvg from "./youtube.svg"
+import EmailSvg from "./email.svg"
 
 export default function Footer() {
   return (
@@ -12,18 +14,28 @@ export default function Footer() {
       <ul id="socialmedia">
         <h2>Follow us at: </h2>
         <li aria-label="Twitter">
-          <a href="https://twitter.com">
+          <a href="https://twitter.com/mayafilmz">
             <TwitterSvg />
           </a>
         </li>
         <li aria-label="Instagram">
-          <a href="https://instagram.com">
+          <a href="https://www.instagram.com/mayafilmz/">
             <InstagramSvg />
           </a>
         </li>
-        <li aria-label="Facebook">
-          <a href="https://facebook.com">
-            <FacebookSvg />
+        <li aria-label="Linked In">
+          <a href="https://www.linkedin.com/in/juan-maya-hernandez-075aa316b/">
+            <LinkedInSvg />
+          </a>
+        </li>
+        <li aria-label="Youtube">
+          <a href="https://www.youtube.com/channel/UCOKvLVp6g2ID4pGJQgegc4A?view_as=subscriber/">
+            <YoutubeSvg />
+          </a>
+        </li>
+        <li aria-label="Email">
+          <a href="mailto:mayafilmsphotovideo@gmail.com">
+            <EmailSvg />
           </a>
         </li>
       </ul>
