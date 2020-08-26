a4e2d4ef9a2efa240f41b34e959a37ac551345f6
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-08-25 23:29:19 -0700

    Edit styles

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,8 @@
 import styled from "styled-components"
 
 export default styled.div`
-  background: white;
+  background: #fdfdfd;
   border-radius: 0.8rem;
-  box-shadow: 0.1rem 0.1rem 1rem rgba(0, 0, 0, 0.2);
   padding: 2rem 1rem;
 
   display: flex;
@@ -11,63 +10,40 @@ export default styled.div`
   align-items: center;
   justify-content: space-between;
 
-  color: var(--color-text-dark);
+  color: ${({ color }) => color};
 
   & > *:not(:last-child) {
     margin-bottom: 2rem;
   }
 
-  &&& h1 {
+  h1 {
     text-decoration: underline;
-    text-decoration-color: var(--color-text-dark);
+    text-decoration-color: ${({ color }) => color};
     color: var(--color-primary);
-    /* font-weight: 700; */
-    font-size: 2rem;
+    font-weight: 700;
+    font-size: 3rem;
   }
 
   button {
-    background: var(--color-primary);
+    background: ${({ color }) => color};
     border-radius: 4rem;
-    color: white;
+    color: #fff;
     padding: 1.2rem 2rem;
-    font-size: clamp(1.8rem, 2vw, 2.5rem);
     &:hover {
       cursor: pointer;
     }
   }
 
   .price {
-    font-size: 2rem;
-    font-weight: 600;
+    font-size: 4.2rem;
+    font-weight: 500;
     color: var(--color-primary);
+    text-decoration: underline wavy;
+    text-decoration-color: ${({ color }) => color};
   }
 
-  .pricing-link {
-    color: var(--color-text-dark);
-    font-size: 1.6rem;
-    padding: 0.5rem 1.2rem;
-    position: relative;
-    overflow: hidden;
-    transition: color 0.3s ease;
-    z-index: 10;
-    &:before {
-      position: absolute;
-      top: 0;
-      left: 0;
-      content: "";
-      transform: translateY(100%);
-      transition: transform 0.3s ease;
-      height: 100%;
-      width: 100%;
-      background: var(--color-secondary);
-      z-index: -1;
-    }
-
-    &:hover {
-      color: white;
-      &:before {
-        transform: translateY(0);
-      }
-    }
+  transition: box-shadow 0.2s ease;
+  &:hover {
+    box-shadow: 1rem 1rem ${({ color }) => color};
   }
 `
