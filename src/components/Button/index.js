2bb3f2d9c0ca9c676607d7c3da0b236cab07f2f4
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-08-25 19:57:21 -0700

    Create Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+  background: ${({ bgColor }) => (bgColor ? bgColor : "var(--color-primary)")};
+  border-radius: 4rem;
+  color: ${({ color }) => (color ? color : "white")};
+  font-size: 2rem;
+  padding: 1rem 2rem;
+  transition: all 0.15s ease-out;
+
+  &:hover,
+  &:focus {
+    cursor: pointer;
+    box-shadow: 0 0.4rem 1rem rgba(0, 0, 0, 0.4);
+    transform: translateY(-0.4rem);
+  }
+
+  &:active {
+    box-shadow: 0 0.2rem 0.7rem rgba(0, 0, 0, 0.7);
+    transform: translateY(-0.2rem);
+  }
+`
+export default Button
