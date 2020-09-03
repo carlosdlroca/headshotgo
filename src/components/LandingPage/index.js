0686a25280df524c5d315b439db397ff5b07c73f
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-02 17:11:14 -0700

    Create LandingPage grid

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,103 @@
+import styled from "styled-components"
+
+export const ImageGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-rows: repeat(8, 0.2fr);
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+
+  [class*="Image"] {
+    object-fit: cover;
+    object-position: 10%;
+  }
+
+  .gatsby-image-wrapper {
+    width: 100%;
+    height: 100%;
+
+    img,
+    picture {
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  .Image1 {
+    grid-area: 1 / 1 / 5 / 3;
+  }
+  .Image2 {
+    grid-area: 5 / 1 / 9 / 3;
+  }
+  .Image3 {
+    grid-area: 1 / 9/ 5 / 11;
+  }
+  .Image4 {
+    grid-area: 5 / 9/ 9 / 11;
+  }
+
+  .Image5 {
+    grid-area: 1 / 3 / 3 / 5;
+  }
+
+  .Image6 {
+    grid-area: 1 / 7/ 3 / 9;
+  }
+
+  .Image7 {
+    grid-area: -3 / 3 / -1 / 5;
+  }
+
+  .Image8 {
+    grid-area: -3 / 7/ -1 /9;
+  }
+
+  @media only screen and (max-width: 91em) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 1.5fr 1fr 1.5fr;
+
+    .Image1 {
+      grid-area: 1 / 1 / 2 / 2;
+    }
+    .Image2 {
+      grid-area: 3 / 1 / 4 / 2;
+    }
+    .Image3 {
+      grid-area: 1 / 3/ 2 / 4;
+    }
+    .Image4 {
+      grid-area: 3 / 3 / 4 / 4;
+    }
+
+    .Image5 {
+      grid-area: 1 / 2 / 2 / 3;
+    }
+
+    .Image6 {
+      grid-area: 3 / 2 / 4 / 3;
+    }
+
+    .Image7,
+    .Image8 {
+      display: none;
+      visibility: hidden;
+    }
+  }
+
+  @media only screen and (max-width: 50em) {
+    grid-template-rows: 0.4fr 1fr 0.4fr;
+    grid-column-gap: 0.3rem;
+    .Image2,
+    .Image3,
+    .Image4 {
+      object-position: 45%;
+    }
+  }
+`
+export const MiddleContent = styled.section`
+  grid-area: 3 / 3 / 7 / 9;
+
+  @media only screen and (max-width: 91em) {
+    grid-area: 2 / 1/ 3 / 4;
+  }
+`
