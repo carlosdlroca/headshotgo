74c4d3fc2f337a1163718a33ef05415e26e3d58c
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-02 17:11:50 -0700

    Edit font-size

--- src/components/TextContainer/index.js
+++ src/components/TextContainer/index.js
@@ -17,7 +17,7 @@ export default styled.section`
 
   p {
     color: var(--color-text-dark);
-    font-size: clamp(2.1rem, 2vw, 2.3rem);
+    font-size: clamp(1.8rem, 2vw, 2rem);
     line-height: 1.7;
     margin: 0 auto 2rem;
     width: min(85vw, 75rem);
