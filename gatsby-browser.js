d12186f9308135be3b93b7639f4895d5b814dea4
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-05 13:59:42 -0700

    Add Intersection-Observer polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,3 +11,9 @@ import Layout from "./src/components/Layout"
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 )
+
+export const onClientEntry = async () => {
+  if (typeof IntersectionObserver == "undefined") {
+    await import("intersection-observer")
+  }
+}
