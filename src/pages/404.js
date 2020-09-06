dbdec3857c0c757685dcd6db8613461f098071f7
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-06 13:20:20 -0700

    Edit wording on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,7 +10,7 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <TextContainer>
       <h1>Oops! Incorrect Page!</h1>
-      <p>Looks like we showed you the incorrect page.</p>
+      <p>Looks like we showed you an incorrect page.</p>
       <p>Please take a look at our work: </p>
       <Link to="/portfolio">
         <Button>Our Work</Button>
