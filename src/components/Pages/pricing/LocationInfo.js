b9d3622e074e4d73554d734e05ceea164fd8ebb9
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-15 22:44:11 -0700

    General css edits

--- src/components/Pages/pricing/LocationInfo.js
+++ src/components/Pages/pricing/LocationInfo.js
@@ -1,16 +1,30 @@
 import styled from "styled-components"
 
 export const LocationInfo = styled.section`
+  background: var(--color-quaternary);
+  color: var(--color-text-dark);
   margin: 7rem 0 0;
   padding: 5rem;
-  background: var(--color-quaternary);
   line-height: 1.8;
-  font-size: 1.6rem;
   h1 {
-    font-size: 2.5rem;
+    font-size: clamp(2.8rem, 3vw, 3.1rem);
     text-align: center;
   }
   li {
     margin-bottom: 1rem;
+    font-size: clamp(2rem, 2.3vw, 2.2rem);
+    strong {
+      text-decoration: underline;
+      text-decoration-color: var(--color-primary);
+    }
+    p {
+      color: var(--color-text-light);
+      font-size: 0.96em;
+      font-style: italic;
+      &:before {
+        content: "* ";
+        font-size: 1.1em;
+      }
+    }
   }
 `
--- src/components/Pages/pricing/PricingCard.js
+++ src/components/Pages/pricing/PricingCard.js
@@ -3,64 +3,80 @@ import styled from "styled-components"
 export const PricingCards = styled.div`
   display: grid;
   grid-row-gap: 3rem;
-  width: 65%;
-  margin: 10rem auto;
+  margin: 10rem auto 5rem;
+  width: min(97%, 120rem);
 
-  @media only screen and (max-width: 31.25em) {
-    width: 97%;
+  .LocationComment {
+    font-size: 2.2rem;
+    color: var(--color-text-light);
   }
 
-  @media only screen and (min-width: 65em) {
-    width: min(90%, 110rem);
+  @media only screen and (max-width: 52.5em) and (min-width: 40em) {
+    width: 60%;
+  }
+
+  @media only screen and (min-width: 52.5em) {
     grid-template-columns: repeat(3, 1fr);
-    grid-column-gap: 4rem;
+    grid-column-gap: 2.3vw;
   }
 `
 
 export const PricingCard = styled.div`
   box-shadow: 0.1rem 0.3rem 1rem rgba(0, 0, 0, 0.5);
-  border-radius: 1rem;
+  border-radius: 0.5rem;
   overflow: hidden;
+  background: #fff;
   .Main {
-    color: white;
-    padding: 3rem 1.4rem;
-    background-color: ${props => props.color};
+    color: var(--color-text-light);
+    padding: 3rem 1.4rem 0;
     position: relative;
     line-height: 1.7;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     h1 {
-      text-align: center;
-      font-size: 2.1rem;
+      font-size: clamp(2.5rem, 2.5vw, 3rem);
       margin-bottom: 2rem;
-    }
-
-    h2 {
-      font-size: 1.7rem;
+      text-decoration: underline;
+      text-decoration-color: ${({ color }) => color};
+      text-align: center;
     }
 
     p {
-      line-height: 1.3;
       font-size: 1.4rem;
+      line-height: 1.3;
       text-align: center;
     }
+
     .Price {
+      font-size: 4.6rem;
       text-align: center;
-      font-size: 4.5rem;
+      sup {
+        font-size: 0.5em;
+      }
     }
+  }
+  .BookNowLink {
+    margin: 2rem 0;
   }
   button {
-    position: absolute;
-    bottom: -2rem;
-    left: calc(50% - 6.6rem);
-    border: 0.2rem solid ${({ color }) => color};
+    color: white;
+    background-color: ${({ color }) => color};
   }
 
+  .psRetouch {
+    font-weight: 700;
+    text-decoration: underline;
+    text-decoration-color: var(--color-text-dark);
+    color: ${({ color }) => color};
+  }
   @media only screen and (max-width: 31.25em) {
     & > *:not(:last-child) {
       margin-bottom: 2rem;
     }
   }
 
-  @media only screen and (min-width: 65em) {
+  @media only screen and (min-width: 52.5em) {
     &:nth-of-type(2n) {
       transform: translateY(-4rem);
     }
@@ -70,8 +86,8 @@ export const PricingCard = styled.div`
 export const PricingInfo = styled.div`
   background: white;
   color: var(--color-text-light);
-  font-size: 1.6rem;
+  font-size: 1.8rem;
   line-height: 1.7;
-  padding: 3.5rem 1rem 3rem;
+  padding: 0 1rem 3rem;
   text-align: center;
 `
