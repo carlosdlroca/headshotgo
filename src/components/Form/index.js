c3cd04eb038db9d8c2be8c837ea60b4f860c8234
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-15 22:45:33 -0700

    Create Form components

---
+++ src/components/Form/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const Form = styled.form``
+export const InputGroup = styled.p``
+export const Label = styled.label``
+export const Input = styled.input``
+export const TextArea = styled.textarea``
