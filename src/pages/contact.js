af07596691f4f2e6d01218c45b0991ca39a5a386
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-20 22:33:24 -0700

    Add properties to html elements

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import SEO from "../components/seo"
+import PageTitle from "../PageComponents/PageTitle"
 import { ContactPageContainer } from "../PageComponents/Contact"
 import { Form, InputGroup, Label, Input, TextArea } from "../components/Form"
 import Button from "../components/Button"
@@ -8,29 +9,34 @@ export default function ContactPage() {
   return (
     <ContactPageContainer>
       <SEO title="Contact" />
+      <PageTitle>Contact Us</PageTitle>
       <Form name="contact" method="POST" data-netlify="true">
         <InputGroup>
           <Label>
             Name
-            <Input type="text" name="name" />
+            <Input type="text" name="name" required />
           </Label>
         </InputGroup>
         <InputGroup>
           <Label>
             Email
-            <Input type="email" name="email" />
+            <Input type="email" name="email" required />
           </Label>
         </InputGroup>
         <InputGroup>
           <Label>
             Phone Number
-            <Input type="tel" name="phone-number" />
+            <Input type="tel" name="phone-number" required />
           </Label>
         </InputGroup>
         <InputGroup>
           <Label>
             Message
-            <TextArea name="message"></TextArea>
+            <TextArea
+              name="message"
+              required
+              placeholder="Ask us anything..."
+            ></TextArea>
           </Label>
         </InputGroup>
         <InputGroup>
