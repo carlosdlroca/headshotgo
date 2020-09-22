1cc1de79d54dba1b7927b0c3bb6ae002096d231b
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-22 16:39:56 -0700

    Remove redundant renderIcon functionality

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -3,43 +3,16 @@ import { useStaticQuery, graphql } from "gatsby"
 import { FooterContainer, Copyright } from "./Styles"
 
 import Logo from "../../../images/HeadshotsOnTheGo.png"
-
-import TwitterSvg from "../../../images/socialmedia/twitter.svg"
-import InstagramSvg from "../../../images/socialmedia/instagram.svg"
-import LinkedInSvg from "../../../images/socialmedia/linkedin.svg"
-import FacebookSvg from "../../../images/socialmedia/facebook.svg"
-import YoutubeSvg from "../../../images/socialmedia/youtube.svg"
-import EmailSvg from "../../../images/socialmedia/email.svg"
-
-const renderSvg = name => {
-  switch (name) {
-    case "instagram":
-      return <InstagramSvg />
-    case "facebook":
-      return <FacebookSvg />
-    case "linkedin":
-      return <LinkedInSvg />
-    case "email":
-      return <EmailSvg />
-    case "twitter":
-      return <TwitterSvg />
-    case "youtube":
-      return <YoutubeSvg />
-    default:
-      return null
-  }
-}
+import renderIcon from "../../../utils/renderIcon"
 
 export default function Footer() {
   const data = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
-          socialMedia {
-            instagram
-            linkedin
-            email
-            facebook
+          links {
+            linkType
+            linkUrl
           }
         }
       }
@@ -47,10 +20,10 @@ export default function Footer() {
   `)
 
   const renderLinks = data => {
-    const { socialMedia } = data.site.siteMetadata
-    return Object.keys(socialMedia).map(name => (
-      <li aria-label={name} key={name}>
-        <a href={socialMedia[name]}>{renderSvg(name)}</a>
+    const { links } = data.site.siteMetadata
+    return links.map(({ linkType, linkUrl }) => (
+      <li aria-label={linkType} key={linkType}>
+        <a href={linkUrl}>{renderIcon(linkType)}</a>
       </li>
     ))
   }
