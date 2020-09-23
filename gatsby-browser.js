3b15f46afc5ccc117c32e787de1edaa41413892f
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-23 13:44:39 -0700

    Move up the Layout folder out of components folder

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,7 @@
 
 // You can delete this file if you're not using it
 import React from "react"
-import Layout from "./src/components/Layout"
+import Layout from "./src/Layout"
 
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
