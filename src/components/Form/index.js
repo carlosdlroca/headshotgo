e02b3734d00d1e1c62741e13baea139baab8ae01
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-09-24 19:04:21 -0700

    Adjust box-shadow

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,7 +6,7 @@ export const Form = styled.form`
 
   button {
     border-radius: 0.4rem;
-    box-shadow: 0 1rem 0.1rem rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0.4rem 0.3rem rgba(0, 0, 0, 0.4);
     @media only screen and (max-width: 50em) {
       font-size: 3rem;
     }
